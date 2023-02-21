89b21ffb8d47d452d29a9571d07bf8471c360713
Pedro Coelho <pcdr@cesar.school>
2023-02-21 01:02:05 -0300

    codeacademy 1 rearrenges

--- exercicios/mini_calendar_ca.c
+++ exercicios/mini_calendar_ca.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+int days_in_month[]={0,31,28,31,30,31,30,31,31,30,31,30,31};
 bool is_leap_year(int year){
   if(year%4!=0){
     return 0;
@@ -14,9 +15,6 @@ bool is_leap_year(int year){
     }
   }
 }
-
-int days_in_month[]={0,31,28,31,30,31,30,31,31,30,31,30,31};
-
 void add_days_to_date(int *mm, int *dd, int *yy, int days_left_to_add){
   int days_left_in_month;
   while (days_left_to_add>0){
@@ -39,7 +37,6 @@ void add_days_to_date(int *mm, int *dd, int *yy, int days_left_to_add){
     }
   }
 }
-
 int main() {
   int mm, dd, yy, days_left_to_add;
   printf("Year between 1800 and 10000(mm dd yy days_add): \n");
