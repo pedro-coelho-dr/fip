fe134fbe8542cc0b5e300ad0a2f6f6afda19a252
Pedro Coelho <pcdr@cesar.school>
2023-03-01 16:03:13 -0300

    final presentation error still there in beecrowd

--- q1_pcdr.c
+++ q1_pcdr.c
@@ -8,9 +8,9 @@ int main() {
     // M (total de tipos de material) -> 1 até M =< 2000
     // C (número de campi) -> 1 até C =< 100
     // 1 linha -> G A M C
-    // 1 matrix -> graduação X numero de atividades
-    // 2 matrix -> atividaes x tipos de material
-    // 3 matrix -> tipos de material x custo do material no campi
+    // 1 matrix -> gxa
+    // 2 matrix -> axm
+    // 3 matrix -> mxc
 
     scanf("%d %d %d %d/n", &G, &A, &M, &C);
 
@@ -21,27 +21,28 @@ int main() {
     int tabela12[G][M];
     int tabela123[G][C];
 
-//tabela 1
+    //tabela1
     for(int g=0; g<G; g++) {
         for (int a=0; a<A;a++){
             scanf("%d",&tabela1[g][a]);
         }
     }
 
-// tabela2
+
+    // tabela2
     for(int a=0; a<A; a++) {
         for (int m=0; m<M;m++){
-            scanf("%d",&tabela1[a][m]);
+            scanf("%d",&tabela2[a][m]);
         }
     }
 
-// tabela3
+    // tabela3
     for(int m=0; m<M; m++) {
         for (int c=0; c<C;c++){
-            scanf("%d",&tabela1[m][c]);
+            scanf("%d",&tabela3[m][c]);
         }
     }
-// multiplicação da tabela 1 pela 2
+    // tabela12 = tabela1*2
     for(int i=0; i<G;i++){
         for(int j=0; j<M;j++){
             tabela12[i][j]=0;
@@ -50,7 +51,7 @@ int main() {
             }
         }
     }
-// multiplicação da tabela12 pela tabela 3
+    // tabela123=tabela12*tabela3
     for(int i=0; i<G;i++){
         for(int j=0; j<C;j++){
             tabela123[i][j]=0;
@@ -60,12 +61,44 @@ int main() {
         }
     }
 
-// printar a tabela123
+    // acha o maior número da coluna
+
+    int maior=0;
+    int maiores[G];
+        for (int i=0; i<G;i++){
+            for (int j=0; j<C; j++){
+                if (tabela123[j][i]>maior){
+                    maior=tabela123[j][i];
+                }
+            }
+            maiores[i]=maior;
+            maior=0;
+        }
+
+    //contar os digitos dos maiores número.
+
+    int digitos[G];
+    int contador=0;
+        for (int i=0;i<G;i++){
+            do {
+                maiores[i]/=10;
+                contador++;
+            } while (maiores[i] != 0);
+            digitos[i]=contador;
+            contador=0;
+        }
+
+    // print tabela123
     for(int i=0; i<G;i++){
         for(int j=0; j<C;j++){
-            printf("%d\t", tabela123[i][j]);
+            if (j==(C-1) && i!=(G-1)){
+                printf("%*d\n", digitos[j], tabela123[i][j]);
+            }else if(j==(C-1) && i==(G-1)){
+                printf("%*d", digitos[j], tabela123[i][j]);
+            }else{
+                printf("%*d ", digitos[j], tabela123[i][j]);
+            }
         }
-        printf("\n");
     }
     return 0;
 }
